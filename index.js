9808c0634fc60124784bd5895da7a10e76fbdd45
Michael Nutt <michael@nuttnet.net>
2019-09-30 22:24:09 -0400

    try switching to official github toolkit

--- index.js
+++ index.js
@@ -1,63 +1,36 @@
-const { Toolkit } = require('actions-toolkit');
-const getConfig = require('./utils/config');
-
-const CONFIG_FILENAME = 'pr-lint.yml';
-
-const config = {
-  check_commits: false
-};
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 // Matches [ch49555] and ch49555/branch-name
 const clubhouseRegex = /(\[ch\d+\])|(ch\d+\/)/;
 
-Toolkit.run(
-  async tools => {
-    const { repository, pull_request } = tools.context.payload;
+const { repository, pull_request } = github.context.payload;
 
-    const repoInfo = {
-      owner: repository.owner.login,
-      repo: repository.name,
-      ref: pull_request.head.ref
-    };
+const repoInfo = {
+  owner: repository.owner.login,
+  repo: repository.name,
+  ref: pull_request.head.ref
+};
 
-    const toSearch = [];
+const toSearch = [];
 
-    const { title, body } = pull_request;
+const { title, body } = pull_request;
 
-    toSearch.push(`PR title: ${title}`);
-    toSearch.push(`PR body: ${body}`);
+toSearch.push(`PR title: ${title}`);
+toSearch.push(`PR body: ${body}`);
 
-    const headBranch = pull_request.head.ref.toLowerCase();
+const headBranch = pull_request.head.ref.toLowerCase();
 
-    toSearch.push(`Branch name: ${headBranch}`);
+toSearch.push(`Branch name: ${headBranch}`);
 
-    // if (config.check_commits) {
-    //   const listCommitsParams = {
-    //     owner: repository.owner.login,
-    //     repo: repository.name,
-    //     pull_number: pull_request.number
-    //   };
-    //   const commitsInPR = (await tools.github.pulls.listCommits(listCommitsParams)).data;
-    //   const messages = commitsInPR.map(commit => commit.commit.message);
-    //   messages.forEach(m => toSearch.push(m));
-    // }
+const passed = toSearch.some(line => {
+  const linePassed = !!line.match(clubhouseRegex);
+  core.warning(`Searching ${line}...${linePassed}`);
+  return linePassed;
+});
 
-    const passed = toSearch.some(line => {
-      const linePassed = !!line.match(clubhouseRegex);
-      tools.log(`Searching ${line}...${linePassed}`);
-      return linePassed;
-    });
+console.log(`Passed clubhouse number check: ${passed}`);
 
-    tools.log(`Passed clubhouse number check: ${passed}`);
-
-    if (passed) {
-      tools.exit.success();
-    } else {
-      tools.exit.failure('PR Linting Failed');
-    }
-  },
-  {
-    event: ['pull_request.opened', 'pull_request.edited', 'pull_request.synchronize']
-    //secrets: ['GITHUB_TOKEN']
-  }
-);
+if (!passed) {
+  core.setFailed('PR Linting Failed');
+}
