484e21ad10211b8862630951a11cc871386bdaa4
Michael Nutt <michael@nuttnet.net>
2019-09-30 22:48:41 -0400

    make action testable

--- index.js
+++ index.js
@@ -1,36 +1,4 @@
-const core = require('@actions/core');
 const github = require('@actions/github');
+const action = require('./lib/action');
 
-// Matches [ch49555] and ch49555/branch-name
-const clubhouseRegex = /(\[ch\d+\])|(ch\d+\/)/;
-
-const { repository, pull_request } = github.context.payload;
-
-const repoInfo = {
-  owner: repository.owner.login,
-  repo: repository.name,
-  ref: pull_request.head.ref
-};
-
-const toSearch = [];
-
-const { title, body } = pull_request;
-
-toSearch.push(`PR title: ${title}`);
-toSearch.push(`PR body: ${body}`);
-
-const headBranch = pull_request.head.ref.toLowerCase();
-
-toSearch.push(`Branch name: ${headBranch}`);
-
-const passed = toSearch.some(line => {
-  const linePassed = !!line.match(clubhouseRegex);
-  core.warning(`Searching ${line}...${linePassed}`);
-  return linePassed;
-});
-
-console.log(`Passed clubhouse number check: ${passed}`);
-
-if (!passed) {
-  core.setFailed('PR Linting Failed');
-}
+action(github.context);
