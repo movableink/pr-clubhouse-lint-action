752503a90157893d17430034b38e093f2f935d9d
Michael Nutt <michael@nuttnet.net>
2019-10-17 23:56:17 -0400

    try having node install its own modules...

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const child_process = require('child_process');
+child_process.execSync('npm install', { stdio: [0, 1, 2] });
+
 const github = require('@actions/github');
 const action = require('./lib/action');
 
