934a713ec2acc38961bc216d4f7021ece6b0ca35
Michael Nutt <michael@nuttnet.net>
2019-10-17 23:58:42 -0400

    run npm in correct dir

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const child_process = require('child_process');
-child_process.execSync('npm install', { stdio: [0, 1, 2] });
+child_process.execSync('npm install', { stdio: [0, 1, 2], cwd: __dirname });
 
 const github = require('@actions/github');
 const action = require('./lib/action');
