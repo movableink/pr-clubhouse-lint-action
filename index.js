22e469e293714a895170722ca21b0c5b58d17c6f
Michael Nutt <michael@nuttnet.net>
2019-10-18 00:12:35 -0400

    try using octokit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const child_process = require('child_process');
-child_process.execSync('npm install', { stdio: [0, 1, 2], cwd: __dirname });
+const fs = require('fs');
+if (!fs.existsSync(`${__dirname}/node_modules`)) {
+  const child_process = require('child_process');
+  child_process.execSync('npm install', { stdio: [0, 1, 2], cwd: __dirname });
+}
 
 const github = require('@actions/github');
 const action = require('./lib/action');
 
-action(github.context);
+action(github.context, github);
