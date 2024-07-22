bbb9d61d848f0d3d31c0764ff9dd4392e7af3263
Alec Rajeev <13004609+alecrajeev@users.noreply.github.com>
2024-07-22 16:44:21 -0400

    test1

--- src/main.js
+++ src/main.js
@@ -6,11 +6,11 @@ async function run() {
   console.log(`Event Name: ${github.context.eventName}`); // Debugging: Log the event name
 
   // Check if the event is a pull request event or pull request target event
-  if (github.context.eventName === 'pull_request' || github.context.eventName === 'pull_request_target') {
-    action(github.context, octokit);
-  } else {
-    console.log('This action only runs on pull request events.');
-  }
+  // if (github.context.eventName === 'pull_request' || github.context.eventName === 'pull_request_target') {
+  //   action(github.context, octokit);
+  // } else {
+  //   console.log('This action only runs on pull request events.');
+  // }
 }
 
 module.exports = {
