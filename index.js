fc44fa6a8c595dd0ec6c0eda5f1c4b1887ac2c4c
Duncan Schulze <13733349+duncaan@users.noreply.github.com>
2024-07-29 14:42:23 -0400

    Use @vercel/ncc to compile the action [sc-104807]

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // When using a release branch, we have node_modules vendored. But on other
 // branches we don't, so when we're testing we want to optionally just run npm
 // to install dependencies
-const fs = require('fs');
-if (!fs.existsSync(`${__dirname}/node_modules`)) {
-  const child_process = require('child_process');
-  child_process.execSync('npm install', { stdio: [0, 1, 2], cwd: __dirname });
-}
+// const fs = require('fs');
+// if (!fs.existsSync(`${__dirname}/node_modules`)) {
+//   const child_process = require('child_process');
+//   child_process.execSync('npm install', { stdio: [0, 1, 2], cwd: __dirname });
+// }
 
 const github = require('@actions/github');
 const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
