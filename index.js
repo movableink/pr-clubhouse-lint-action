14664e83a41f5c027c3a654b6d0f75f0b60ad02a
Duncan Schulze <13733349+duncaan@users.noreply.github.com>
2026-03-09 21:28:22 -0400

    support bot bypass of checks upgrade to node 24 Move to ESM based on dependencies

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // When using a release branch, we have node_modules vendored. But on other
 // branches we don't, so when we're testing we want to optionally just run npm
 // to install dependencies
-const fs = require('fs');
-if (!fs.existsSync(`${__dirname}/node_modules`)) {
-  const child_process = require('child_process');
-  child_process.execSync('npm install', { stdio: [0, 1, 2], cwd: __dirname });
+import { existsSync } from 'fs';
+import { execSync } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+if (!existsSync(`${__dirname}/node_modules`)) {
+  execSync('npm install', { stdio: [0, 1, 2], cwd: __dirname });
 }
 
-const github = require('@actions/github');
-const action = require('./lib/action');
+const { context, getOctokit } = await import('@actions/github');
+const { default: action } = await import('./lib/action.js');
 
-const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
-
-action(github.context, octokit);
+action(context, getOctokit(process.env.GITHUB_TOKEN));
